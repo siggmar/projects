2ecaae1b3f631b794ea56c8f3bc0d77484182606
siggmar <subberman0822@gmail.com>
2025-08-28 22:37:01 +0200

    basic config changes

--- dwm/config.h
+++ dwm/config.h
@@ -1,25 +1,30 @@
 /* See LICENSE file for copyright and license details. */
 
+enum { SchemeNorm, SchemeSel, SchemeTag, SchemeTagSel, SchemeStatus, SchemeLast };
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char *fonts[]          = { "FiraMonoNerdFont-Bold:size=14" };
+static const char dmenufont[]       = { "FiraMonoNerdFont-Bold:size=14" };
+static const char col_gray1[] = "#2d2d2d"; 
+static const char col_gray2[] = "#000000";
+static const char col_gray3[] = "#9c9a9a";
+static const char col_gray4[] = "#2d2d2d";
+static const char col_cyan[]  = "#808fa0";
+static const char *colors[SchemeLast][3]      = {
+		/*               fg         bg         border   */
+	[SchemeNorm]    = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]     = { col_gray4, col_gray4, col_cyan  },
+    [SchemeTag]     = { col_gray3, col_gray1, col_gray1 },
+    [SchemeTagSel]  = { col_gray1, col_cyan,  col_cyan  },
+    [SchemeStatus]  = { col_gray3, col_gray1, col_gray1 },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -40,8 +45,8 @@ static const int refreshrate = 120;  /* refresh rate (per second) for client mov
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      NULL  },    /* first entry is default */
+	{ "><>",      tile },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -66,7 +71,7 @@ static const char *termcmd[]  = { "st", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },
 	{ 0, XF86XK_MonBrightnessUp,    		   spawn,          SHCMD("brightnessctl set 10%+") },
     { 0, XF86XK_MonBrightnessDown,             spawn,          SHCMD("brightnessctl set 10%-")  },
     { 0, XF86XK_AudioMute,                     spawn,          SHCMD("amixer set Master toggle") },
